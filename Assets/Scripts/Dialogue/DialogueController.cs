86cf0c7593c35da2af6ef9e2afade12dbe277b6b
sadovnichek <kapkaevictor@gmail.com>
2021-04-17 17:39:53 +0500

    add a start items

---
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts.Common;
+using Assets.Scripts.Player.Movement.Configs;
+using Assets.Scripts.Player.Movement.Helpers;
+using Assets.Scripts.Player.Movement.Services;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using UnityEngine;
+
+
+namespace Assets.Scripts.Dialogue
+{
+    public class DialogueController : ControllerBase
+    {
+        private readonly List<ServiceBase> services;
+
+        public DialogueController(GameObject gameObject, IServiceProvider serviceProvider): base(gameObject, serviceProvider)
+        {
+            services = new List<ServiceBase>();
+        }
+
+        [UsedImplicitly]
+        public override void Update()
+        {
+            foreach(var service in services)
+                service.Update();
+        }
+
+        [UsedImplicitly]
+        public override void FixedUpdate()
+        {
+            foreach (var service in services)
+                service.FixedUpdate();
+        }
+    }
+}
