e8620999a40c5eae9ebc9d3f21fbc5b45740038c
sadovnichek <kapkaevictor@gmail.com>
2021-06-03 22:27:31 +0500

    add menu controller

---
+++ Assets/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+}
